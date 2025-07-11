a67207878d466ad7a5b8598f97e6e3d8a32809b9
HieuDoan11102004 <hieudoan1110@gmail.com>
2025-07-11 13:24:36 +0700

    add class

---
+++ src/main/java/org/example/java_core/Practice/Practice3/Model/Cars.java
@@ -0,0 +1,6 @@
+package org.example.java_core.Practice.Practice3.Model;
+
+public class Cars extends Vehicles{
+    private int numberOfSeats;
+    private String engineType;
+}
---
+++ src/main/java/org/example/java_core/Practice/Practice3/Model/Motorbikes.java
@@ -0,0 +1,5 @@
+package org.example.java_core.Practice.Practice3.Model;
+
+public class Motorbikes extends Vehicles {
+    private float capacity;
+}
